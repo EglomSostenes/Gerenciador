f0a5e9a98194cbf68b0cd6609698fbf46197e48b
eglom.sostenes <eglom.sostenes@cepedi.org.br>
2020-11-15 16:10:02 -0300

    Construção dos scopes de alliances ativos e vencidos, criação do método que soma os custos dos projetos dada uma company

--- app/models/alliance.rb
+++ app/models/alliance.rb
@@ -7,6 +7,7 @@ class Alliance < ApplicationRecord
   validate :interval_valid
 
   scope :expired, ->() {where(Alliance.arel_table[:end_date].lt(Date.today))}
+  scope :active, ->() {where(Alliance.arel_table[:end_date].gt(Date.today))}
 
   def interval_valid
     if start_date and end_date
@@ -16,7 +17,7 @@ class Alliance < ApplicationRecord
 
   def to_s
     if company and institution
-      start_date.year.to_s + ' - ' + company.name + ' | ' + institution.name unless new_record?
+      start_date.year.to_s + ' - ' + end_date.year.to_s + ' - ' + company.name + ' | ' + institution.name unless new_record?
     end
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < Person
     has_many :alliances
     has_many :projects, through: :alliances
+
+    def investment
+        self.projects.sum(:cost)
+    end
 end
